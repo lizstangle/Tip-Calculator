73b3a13bdcfcb77e3994f7c5a0618c4acc66a2c1
lizstangle <lizstangle@me.com>
2020-09-22 12:00:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ console.log(display.innerHTML) // prints the text inside the tags
 console.log(billInput.value) // prints the value entered into the input
 
 billInput.value = 999.88 // Set the value to 999.88
+
+  function calculateTip() {
+    const billValue = billInput.value
+    const tipValue = tipInput.value
+  
+    const tipAmount = billValue * tipValue / 100
+  }
+  
