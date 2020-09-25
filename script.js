df637b3bf881602305eadafdeccec08404d86f48
lizstangle <lizstangle@me.com>
2020-09-25 01:52:22 -0700

    styles

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const display = document.querySelector('#display')
-const billInput = document.querySelector('#bill')
+billInput.addEventListener("input", calculateTip);
+tipInput.addEventListener("input", calculateTip);
+peopleInput.addEventListener("input", calculateTip);
+
+
+
+
+const billInput = document.querySelector('#input-bill')
+const tipInput = document.querySelector('#input-tip')
+const peopleInput = document.querySelector('#input-people')
+const displayTip = document.querySelector('#displayTip')
+const displayTotal = document.querySelector('displayTotal')
+
 const tipInput = document.querySelector('#tip')
 
 console.log(display.innerHTML) // prints the text inside the tags
@@ -9,11 +20,17 @@ billInput.value = 999.88 // Set the value to 999.88
 
   function calculateTip() {
     const billValue = billInput.value
-    const tipValue = tipInput.value
+    const percent = tipInput.value
   
     const tipAmount = billValue * tipValue / 100
 
     display.innerHTML = tipAmount
   }
   calculateTip()
-  
+
+    billInput.addEventListener('input', calculateTip)
+    tipInput.addEventListener('input', calculateTip)
+
+    display.innerHTML = tipAmount.toFixed(2)
+
+
